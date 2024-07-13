67f8a6ff71877e689442c95632e471dcd4eac4e4
mendelson44 <benmendelson1997@gmail.com>
2024-07-13 22:56:43 +0300

    last commit 1

---
+++ app/src/main/java/com/example/gold_miner_game/model/SoundPlayer.java
@@ -0,0 +1,34 @@
+package com.example.gold_miner_game.model;
+
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class SoundPlayer {
+
+    private Context context;
+    private Executor executor;
+    //private MediaPlayer mediaPlayer;
+
+    public SoundPlayer(Context context) {
+        this.context = context.getApplicationContext();
+        this.executor = Executors.newSingleThreadExecutor();
+    }
+
+    public void playSound(int res) {
+        executor.execute(() -> {
+            MediaPlayer mediaPlayer = MediaPlayer.create(context, res);
+            mediaPlayer.setLooping(false);
+            mediaPlayer.setVolume(1.0f, 1.0f);
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(mp -> {
+                mp.stop();
+                mp.release();
+                mp = null;
+            });
+        });
+    }
+}
